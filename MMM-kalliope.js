b06f7deabb75dd2eaedc6c1d9e2f82a6d1066228
dmwilsonkc <38334225+dmwilsonkc@users.noreply.github.com>
2018-07-15 17:42:04 -0500

    Retry if statement for interval

--- MMM-kalliope.js
+++ MMM-kalliope.js
@@ -42,7 +42,12 @@ Module.register('MMM-kalliope',{
 
         for(var i = 0; i < this.messages.length; i++){
             var dif = currentDate.getTime() - this.messages[i].timestamp.getTime();
-            var secondsFromCurrentDateToMessageDate = dif / 1500;
+            if (this.meesage.length > 30){
+              var secondsFromCurrentDateToMessageDate = dif / 1500;
+            }
+            else{
+              var secondsFromCurrentDateToMessageDate = dif / 1000;
+            }
             var secondsBetweenDates = Math.abs(secondsFromCurrentDateToMessageDate);
 
             // delete the message if to old
