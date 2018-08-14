f5586f975fc42ed9170f7ece65eb5abd66fa0903
dmwilsonkc <38334225+dmwilsonkc@users.noreply.github.com>
2018-08-14 18:02:56 -0500

    Change index of message to splice

--- MMM-kalliope.js
+++ MMM-kalliope.js
@@ -106,7 +106,7 @@ Module.register('MMM-kalliope',{
             }
   if (notification == "AUDIO_END"){
             // When Mycroft signals the AUDIO_OUTPUT_END remove the message from the listener
-            this.messages.splice(0, 2);
+            this.messages.splice(1, 2);
 
             }
 
